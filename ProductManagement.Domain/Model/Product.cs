e0847d775a4749ce27e4ea51e22e9141f8e2a55e
Amanda <amandaqn2016@gmail.com>
2025-12-28 15:16:53 -0300

    teste

--- ProductManagement.Domain/Model/Product.cs
+++ ProductManagement.Domain/Model/Product.cs
@@ -10,6 +10,7 @@ namespace ProductManagement.Domain.Model
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        
         public string? Url { get; set; }
         public decimal Valor { get; set; }
     }
